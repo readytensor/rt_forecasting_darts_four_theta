a4250915111d4622cfdc33fc3ab9755c1e5628ea
rt-desai <abhyuday.desai@readytensor.com>
2024-05-30 18:46:30 -0700

    remove gpu-resource tracking from model

--- src/train.py
+++ src/train.py
@@ -10,8 +10,8 @@ from utils import (
     read_csv_in_directory,
     read_json_as_dict,
     set_seeds,
-    ResourceTracker,
     map_hyperparameters,
+    TimeAndMemoryTracker,
 )
 
 logger = get_logger(task_name="train")
@@ -43,7 +43,7 @@ def run_training(
     """
 
     try:
-        with ResourceTracker(logger, monitoring_interval=0.1):
+        with TimeAndMemoryTracker(logger) as _:
             logger.info("Starting training...")
             # load and save schema
             logger.info("Loading and saving schema...")
